1a563a9b7c7e6580a84655d0eb95461e48fb4aaf
Brylle Quinto <quintobrylle23@gmail.com>
2022-06-07 20:39:23 +0800

    Whitelist minting test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { fetchData } from "./redux/data/dataActions";
 import * as s from "./styles/globalStyles";
 import styled from "styled-components";
 import keccak256 from "keccak256";
+import MerkleTree from "merkletreejs";
 
 const truncate = (input, len) =>
   input.length > len ? `${input.substring(0, len)}...` : input;
