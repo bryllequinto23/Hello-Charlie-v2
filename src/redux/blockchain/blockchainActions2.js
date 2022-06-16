7abe23cd36e5e272fadfdc7975771fd5e1628e9a
Brylle Quinto <quintobrylle23@gmail.com>
2022-06-16 17:58:12 +0800

    Added wallet options

--- src/redux/blockchain/blockchainActions2.js
+++ src/redux/blockchain/blockchainActions2.js
@@ -62,7 +62,7 @@ const web3Modal = new Web3Modal({
 export const connect = () => {
   return async (dispatch) => {
     dispatch(connectRequest());
-    console.log('console1')
+    console.log('console2')
     const abiResponse = await fetch("/config/abi.json", {
       headers: {
         "Content-Type": "application/json",
