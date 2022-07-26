bbd62d3b4cf28c927256a5009215132b93295465
Brylle Quinto <quintobrylle23@gmail.com>
2022-07-26 15:18:35 +0800

    try captcha

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import styled from "styled-components";
 import keccak256 from "keccak256";
 import MerkleTree from "merkletreejs";
 import ReCAPTCHA from "react-google-recaptcha";
+import axios from "axios";
 
 const truncate = (input, len) =>
   input.length > len ? `${input.substring(0, len)}...` : input;
